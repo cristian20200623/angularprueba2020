b5004de8da0c2c6a83c609bbf688ab4d03e54666
Cristian <cquiroz@vuce.gob.ar>
2020-06-25 02:26:11 -0300

    test

--- src/app/google-analytics.service.ts
+++ src/app/google-analytics.service.ts
@@ -31,8 +31,17 @@ export class GoogleAnalyticsService {
       'value': eventValue
     });
     */
+   /*
    gtag('event', 'enviar', { 
       'event_category': 'busqueda_barrera', 'event_label': 'inicio', 'value': 'haciendopruebas'});
 
     }
+    */
+   // Configure a target
+gtag('config', 'GA_MEASUREMENT_ID_1');
+gtag('config', 'GA_MEASUREMENT_ID_2', { 'groups': 'agency' });
+gtag('config', 'GA_MEASUREMENT_ID_3', { 'groups': 'agency' });
+
+// Route this sign_in event to Analytics IDs in the 'agency' group:
+gtag('event', 'sign_in', { 'send_to': 'agency' });
 }
